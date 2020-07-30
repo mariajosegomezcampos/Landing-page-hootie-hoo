04f165c8d5a48e56435e2906aeecf2e44ca5babf
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-07-29 21:36:14 -0400

    creando entorno, compilando sass con gulp

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+ 
+
+// funcion scroll ancla 
+ $('nav a').click(function(e){ //Creamos un evento click para el enlace			
+   e.preventDefault();		//evitar el eventos del enlace normal
+   let strAncla=$(this).attr('href'); //id del ancla - le agrego un atributo mediant attr
+      $('body,html').stop(true,true).animate({				
+         scrollTop: $(strAncla).offset().top
+      },2000);
+   
+});
+
+
+
+
+
+//funcion libreria slick
+
+   $('.section-slider').slick({
+     arrows: false
+   });
